f9f7db12448b55041e0400cc73259eae353d5d24
ouranobasis <mahasacham@yahoo.com>
2018-12-17 08:33:45 -0800

    first commit, the app loads Crito and has parsing information in a dialog box

---
+++ App1/App1/SentenceItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1
+{
+    class SentenceItem
+    {
+        public string parseInfo { get; set; }
+        public string item { get; set; }
+    }
+}
