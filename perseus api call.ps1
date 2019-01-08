46fc6d7db88ea0f8f423628fe56f08cd81ac9ab4
ouranobasis <mahasacham@yahoo.com>
2019-01-08 07:43:46 -0800

    added the lemmas to use with api call

---
+++ perseus api call.ps1
@@ -0,0 +1,9 @@
+﻿Get-Content .\missingLemmas.txt | ForEach-Object{
+
+    $response = Invoke-RestMethod -Uri http://www.perseus.tufts.edu/hopper/xmlmorph?lang=greek"&"lookup=$_
+   
+    $_ >> C:\Users\IWANOS\Desktop\lemmaDefinitions.txt
+
+    $response.analyses.analysis.lemma >> C:\Users\IWANOS\Desktop\lemmaDefinitions.txt
+    
+}
