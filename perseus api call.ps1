6d1ba50fb9a8610a8bf3ff2c0323af5e9cdb3173
ouranobasis <mahasacham@yahoo.com>
2019-01-08 21:27:31 -0800

    added my newest lemma definitions which 129 morphemes dont have a lemma and I deleted the powershell script I was using to call the perseus API to fill in some of the missing lemma

--- perseus api call.ps1
+++
@@ -1,9 +0,0 @@
-﻿Get-Content .\missingLemmas.txt | ForEach-Object{
-
-    $response = Invoke-RestMethod -Uri http://www.perseus.tufts.edu/hopper/xmlmorph?lang=greek"&"lookup=$_
-   
-    $_ >> C:\Users\IWANOS\Desktop\lemmaDefinitions.txt
-
-    $response.analyses.analysis.lemma >> C:\Users\IWANOS\Desktop\lemmaDefinitions.txt
-    
-}
